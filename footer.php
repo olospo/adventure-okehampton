209ca8d94f5e5f9e50f05edb377c74a8278eb1de
Tom Brooks <tom@olospo.co.uk>
2019-07-09 14:33:39 +0100

    Footer updates

--- footer.php
+++ footer.php
@@ -1,9 +1,16 @@
 <?php /* Footer */ ?>
-<footer>
+<footer class="social-links"
   <div class="container">
     <div class="copyright-links">
-      <p>&copy; <?php the_date('Y'); ?> Company Name</p>
       <?php wp_nav_menu( array( 'theme_location' => 'footer' ) ); ?>
+      <p>&copy; <?php the_date('Y'); ?> Company Name</p>
+    </div>
+    
+  </div>
+</footer>
+<footer class="accreditations">
+  <div class="container">
+    Accreditation images/links here.
   </div>
 </footer>
 <?php wp_footer(); ?>
