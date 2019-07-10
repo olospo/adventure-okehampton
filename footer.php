ba7d2a44e6d59785f215a462054f9932fdd59a08
Tom Brooks <tom@olospo.co.uk>
2019-07-10 11:00:11 +0100

    Adding in footer sections

--- footer.php
+++ footer.php
@@ -1,11 +1,51 @@
 <?php /* Footer */ ?>
-<footer class="social-links"
+<section class="quick-links">
   <div class="container">
-    <div class="copyright-links">
-      <?php wp_nav_menu( array( 'theme_location' => 'footer' ) ); ?>
-      <p>&copy; <?php the_date('Y'); ?> Company Name</p>
+    <div class="one-third column">
+      <h4>About Us</h4>
+      <p>Now booking for our 2019 daily adventures programme. The most popular activities are gorge scrambling, rock climbing, high ropes and paddlesports.</p>
+      <a href="#" class="button">View All</a>
     </div>
-    
+    <div class="one-third column">
+      <h4>Accommodation</h4>
+      <p>Looking for an exciting weekend break or a fun family activity holiday or just a campsite? We have a range of sites that cater for individuals, families and larger groups.</p>
+      <a href="#" class="button">View All</a>
+    </div>
+    <div class="one-third column">
+      <h4>Special Offers</h4>
+      <p>We are now booking for 2019/20 school activity residentials, activity days and group events. Special offers with reduced prices on our winter.</p>
+      <a href="#" class="button">Call Today</a>
+    </div>
+  </div>
+</section>
+<section class="social">
+  <div class="container">
+    <h3>Get Social</h3>
+  </div>
+</section>
+<section class="newsletter">
+  <div class="container">
+    <h3>Sign up to our newsletter</h3>
+  </div>
+</section>
+
+<footer class="social-links">
+  <div class="container">
+    <div class="one-third column">
+      About Us
+    </div>
+    <div class="one-third column">
+      Follow us
+    </div>
+    <div class="one-third column">
+      How to find us
+    </div>
+    <div class="copyright-links">
+<!--
+      <?php // wp_nav_menu( array( 'theme_location' => 'footer' ) ); ?>
+      <p>&copy; <?php the_date('Y'); ?> Company Name</p>
+-->
+    </div>
   </div>
 </footer>
 <footer class="accreditations">
