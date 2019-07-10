6266c40c596c966b187c41aefee31da7856dc80d
Tom Brooks <tom@olospo.co.uk>
2019-07-10 16:59:19 +0100

    Buttons, Header and Footer

--- footer.php
+++ footer.php
@@ -30,11 +30,14 @@
 <section class="social">
   <div class="container">
     <h3>Get Social</h3>
-    <div class="six columns">
-      Instagram Photos
-    </div>
-    <div class="six columns">
-      Facebook Feed
+    <div class="row">
+      <div class="six columns">
+        Instagram Photos
+      </div>
+      <div class="six columns">
+        <h4>Facebook Feed</h4>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+      </div>
     </div>
   </div>
 </section>
--- templates/home.php
+++ templates/home.php
@@ -2,7 +2,43 @@
 get_header();
 while ( have_posts() ) : the_post(); ?>
 
-<?php get_template_part( 'inc/hero' ); ?>
+<?php /* Hero Section */
+$title = get_field('title');
+$description = get_field('short_description');
+$buttonText = get_field('button_text');
+$linkType = get_field('button_link');
+$internalLink = get_field('button_link_internal');
+$externalLink = get_field('button_link_external');
+$background = get_field('background_image');
+$colourScheme = get_field('colour_scheme');
+?>
+
+<section class="hero <?php echo $colourScheme; ?>" style="background: linear-gradient(rgba(0, 0, 0, 0.45), rgba(0, 0, 0, 0.45)), <?php if($colourScheme == "black"){ ?> url('<?php echo get_template_directory_uri(); ?>/img/header-black.png') center center no-repeat,<?php } if($colourScheme == "blue"){ ?> url('<?php echo get_template_directory_uri(); ?>/img/header-blue.png') center center no-repeat,<?php } ?> url(' <?php echo $background['url']; ?> ') center center no-repeat; background-size: cover; "> 
+  <div class="float">
+    <div class="container">
+      <div class="content eight columns offset-by-two">
+      <h1><?php echo $title; ?></h1>
+      <form>
+        <select class="activity">
+          <option value="" selected>Activity Type</option>
+          <option value="text">Text</option>
+          <option value="text">Text</option>
+          <option value="text">Text</option>
+          <option value="text">Text</option>
+        </select>
+        <select class="party">
+          <option value="" selected>Your Party</option>
+          <option value="text">Text</option>
+          <option value="text">Text</option>
+          <option value="text">Text</option>
+          <option value="text">Text</option>
+        </select>
+        <input type="submit" value="Book Now">
+      </form>
+      </div>
+    </div>
+  </div>
+</section>
 
 <?php
 $thecontent = get_the_content();
@@ -18,7 +54,7 @@ if(!empty($thecontent)) { ?>
 
 <section class="testimonial">
   <div class="container">
-    <blockquote>
+    <blockquote class="eight columns offset-by-two">
       <p>"It was so much fun, I feel like I can climb a mountain!"</p>
       <footer>Thomas, King Edward school, Bath</footer>
     </blockquote>
@@ -27,8 +63,10 @@ if(!empty($thecontent)) { ?>
 
 <section class="activities-list">
   <div class="container">
+    <div class="six columns eight columns offset-by-two">
     <h2>Activities</h2>
     <p>We have 20 activities, ranging from archery to geocaching for you to create your own adventure!</p>
+    </div>
   </div>
 </section>
 
